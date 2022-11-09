0c45d0431cffd64cbc2ebd7af2e5b1237116e914
HARAHO <1438eogh@gmail.com>
2022-11-09 16:47:08 +0900

    first commit

---
+++ pages/sub/subTest001/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function subTest001() {
+  return <div>sub.test001</div>;
+}
---
+++ pages/sub/subTest002/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function subTest002() {
+  return <div>sub.test002</div>;
+}
